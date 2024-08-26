2c54c08c50ff5bd82aeaf0e5cb8f10f3743411d0
eazi201 <kfablionel@gmail.com>
2024-08-26 20:22:17 +0200

    implement onboarding

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
-import { View, Text,ScrollView ,Images} from 'react-native'
+import { View, Text,ScrollView ,Image} from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+
 import { images } from '../constants'
+import CustomButton from '../components/CustomButton'
 
 
 
@@ -12,16 +14,50 @@ import { images } from '../constants'
 const DeeZNutz = () => {
   return (
     <SafeAreaView  className="bg-primary h-full">
-      <ScrollView contentContainerStyle={{ height:'100%'}}>
-        <View className="w-full justify-center items-center h-full px-4">
+      <ScrollView contentContainerStyle={{ height: '100%',}}>
+        <View className="w-full justify-start items-center min-h-[84vh] px-4">
           <Image
-          source={images.logo2}
-          className="w-[130px] b-[84px]"
+          source={images.logo}
+          className="w-[150px] b-[84px]"
           resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[350px] w-full h-[198px]"
+            resizeMode="contain"
+          />
+
+            <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Nutz</Text>
+            </Text>
+
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-4 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Nutz
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
           />
         </View>
       </ScrollView>
-    </SafeAreaView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+      </SafeAreaView>
+    
   )
 }
 
---
+++ components/CustomButton.jsx
@@ -0,0 +1,30 @@
+import { TouchableOpacity,Text,ActivityIndicator } from 'react-native'
+import React from 'react'
+import { isLoading } from 'expo-font'
+
+const CustomButton = ({ title,handlePress,containerStyles, textStyles, IsLoading}) => {
+  return (
+    <TouchableOpacity  className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center`}
+        onPress={handlePress}
+        activeOpacity={0.7}
+        className = {`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles}  $ {isLoading? 'opacity-50 :''} `}
+            
+            disabled={!isLoading}
+        >
+
+
+      <Text className= {`text-primary font-psemibold text-lg ${textStyles}`}>{title}</Text>
+
+      {/* {isLoading && (
+        <ActivityIndicator
+          animating={isLoading}
+          color="#fff"
+          size="small"
+          className="ml-2"
+        /> */}
+      {/* )} */}
+    </TouchableOpacity>
+  )
+}
+
+export default CustomButton
